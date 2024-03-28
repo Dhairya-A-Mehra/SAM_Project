68cff0e7f1a31d084d4f13272d42ba72ffdf5e95
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-03-28 20:27:00 +0530

    Update SAM_Project.cpp

--- SAM_Project.cpp
+++ SAM_Project.cpp
@@ -1,15 +1,15 @@
-const int ir1 = 8; 
-const int ir2 = 10; 
-const int ir3 = 12; 
+const int pir1 = 8; 
+const int pir2 = 10; 
+const int pir3 = 12; 
 const int led1 = 2;    
 const int led2 = 4;
 const int led3 = 6;
 const int ldrPin = A0; 
 
 void setup() {
-  pinMode(ir1, INPUT);
-  pinMode(ir2, INPUT);
-  pinMode(ir3, INPUT);
+  pinMode(pir1, INPUT);
+  pinMode(pir2, INPUT);
+  pinMode(pir3, INPUT);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
@@ -17,39 +17,38 @@ void setup() {
 }
 
 void loop() {
-  int ir1val = digitalRead(ir1);
-  int ir2val = digitalRead(ir2);
-  int ir3val = digitalRead(ir3);
+  int pir1val = digitalRead(pir1);
+  int pir2val = digitalRead(pir2);
+  int pir3val = digitalRead(pir3);
   int ldrValue = analogRead(ldrPin); 
 
   if (ldrValue > 750) { 
-    if (ir1val == LOW && ir2val == HIGH && ir3val == HIGH) {
+    if (pir1val == LOW && pir2val == HIGH && pir3val == HIGH) {
       digitalWrite(led1, HIGH);
       digitalWrite(led2, HIGH);
       digitalWrite(led3, LOW);
-      Serial.println("Motion detected by IR 1!");
+      Serial.println("Motion detected by PIR 1!");
     } 
-    else if (ir1val == HIGH && ir2val == LOW && ir3val == HIGH) {
+    else if (pir1val == HIGH && pir2val == LOW && pir3val == HIGH) {
       digitalWrite(led2, HIGH);
       digitalWrite(led1, LOW);
       digitalWrite(led3, LOW);
-      Serial.println("Motion detected by IR 2!");
+      Serial.println("Motion detected by PIR 2!");
     } 
-    else if (ir1val == HIGH && ir2val == HIGH && ir3val == LOW) {
+    else if (pir1val == HIGH && pir2val == HIGH && pir3val == LOW) {
       digitalWrite(led3, HIGH);
       digitalWrite(led2, HIGH);
       digitalWrite(led1, LOW);
-      Serial.println("Motion detected by IR 3!");
+      Serial.println("Motion detected by PIR 3!");
     } 
-    else if (ir1val == LOW && ir2val == LOW && ir3val == LOW) {
+    else if (pir1val == LOW && pir2val == LOW && pir3val == LOW) {
       digitalWrite(led3, HIGH);
       digitalWrite(led2, HIGH);
       digitalWrite(led1, HIGH);
-      Serial.println("Motion detected by all IRs!");
+      Serial.println("Motion detected by all PIRs!");
     }
   } 
   else {
-   
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
